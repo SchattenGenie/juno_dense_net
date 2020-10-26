fb2dd5ed7e8d3dd22c2df7242e16eb66f5bf2f82
vbelavin <belavin@phystech.edu>
2020-10-26 16:56:27 +0300

    adding more features for training

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -5,10 +5,10 @@ import numpy as np
 
 class JunoLoader:
     def __init__(self, target_variable="Edep"):  # edepX, edepY, edepZ
-        self._columns = ['R_lmpt', 'z_c_lpmt', 'ht_std', 'ht_mean', 'allHits_lpmt']
+        self._columns = ['R_lmpt', 'x_c_lpmt', 'y_c_lpmt', 'z_c_lpmt', 'ht_std', 'ht_mean', 'allHits_lpmt']
         self._target_variable = target_variable
-        self._mean = [8., 0., 76., 122., 9705.]
-        self._std = [3., 6., 10.5, 10., 4851.]
+        self._mean = [8., 0., 0., 0., 76., 122., 9705.]
+        self._std = [3., 6., 6., 6., 10.5, 10., 4851.]
 
     def preprocess_data(self, path):
         df = pd.read_csv(path)
@@ -21,7 +21,7 @@ class JunoLoader:
     def fit_transform(self, path):
         df = self.preprocess_data(path)
         X = (df[self._columns].values - self._mean) / self._std
-        y = df['Edep'].values
+        y = df[self._target_variable].values
         return X, y
 
     def fit(self, path):
