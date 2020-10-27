d89c6293abd55dbd5758236e17f6797d05846232
vbelavin <belavin@phystech.edu>
2020-10-27 21:27:01 +0300

    a small bug fix

--- run_optimization.py
+++ run_optimization.py
@@ -44,7 +44,8 @@ base_command = """python train_model.py --project_name {project_name} \
 --nonlinearity {nonlinearity} --scheduler_type {scheduler_type} \
 --batch_size {batch_size} --epochs {epochs} --use_swa {use_swa} \
 --optimizer_cls {optimizer_cls} --use_layer_norm {use_layer_norm} \
---init_type {init_type} --train_type {train_type} --loss_function {loss_function}"""
+--init_type {init_type} --train_type {train_type} --loss_function {loss_function} \
+--target_variable {target_variable}"""
 
 command_cluster = "sbatch -c {0} -t {1} --gpus={2} --job-name={3} run_command.sh"
 
@@ -56,15 +57,17 @@ command_cluster = "sbatch -c {0} -t {1} --gpus={2} --job-name={3} run_command.sh
 @click.option('--datadir', type=str, default='./')
 @click.option('--project_name', type=str, prompt='Enter project name')
 @click.option('--work_space', type=str, prompt='Enter workspace name')
-@click.option('--max_epochs', type=int, default=5000)
+@click.option('--max_epochs', type=int, default=3000)
 @click.option('--max_processes_in_parallel', type=int, default=3)
 @click.option('--train_type', type=str, default="0")  # 0 20 3 23
-@click.option('--train_nets_on_one_gpu', type=int, default=7)  # only for slurm
+@click.option('--train_nets_on_one_gpu', type=int, default=10)  # only for slurm
+@click.option('--target_variable', type=str, default="energy") # energy, vertex
 def run_optimization(
         project_name, work_space,
         slurm=False, datadir="./", slurm_username="vbelavin",
         algorithm="bayes", max_processes_in_parallel=3,
-        train_nets_on_one_gpu=3, max_epochs=5000, train_type="0"
+        train_nets_on_one_gpu=3, max_epochs=5000, train_type="0",
+        target_variable="energy"
 ):
     optimizer_config["algorithm"] = algorithm
     optimizer = Optimizer(optimizer_config, project_name=project_name)
@@ -78,6 +81,7 @@ def run_optimization(
             work_space=work_space,
             datadir=datadir,
             train_type=train_type,
+            target_variable=target_variable,
             **parameters["parameters"]
         )
         print(command_to_run)
@@ -96,7 +100,7 @@ def run_optimization(
                 with open("run_command.sh", "w") as file:
                     file.write(base_slurm_command.format(" &\n".join(commands_to_run) + " &\nwait"))
                     process = subprocess.Popen(
-                        command_cluster.format(3, 60 * 12, 1, "juno_dense_net_opt"),  # 1 cpu, 12 hours, 1 gpu
+                        command_cluster.format(3, 60 * 20, 1, "juno_dense_net_opt"),  # 3 cpu, 20 hours, 1 gpu
                         shell=True,
                         close_fds=True,
                         stdout=subprocess.DEVNULL,
